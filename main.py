bd7eebcd2e6dac5b5c7792bb1ca2c1752628c140
dharc <dharc@outlook.com>
2025-08-19 22:28:26 +0800

    1

--- main.py
+++ main.py
@@ -27,4 +27,13 @@ class dnftools(Star):
         '''
         result = lucky_channel.list_all_channels_and_provinces()
         yield event.plain_result(result) # 发送一条纯文本消息
-    
+    @filter.command("幸运频道")
+    async def luckychannel111(self, event: AstrMessageEvent): # type: ignore
+        '''
+        列出当前的频道和大区对应的序号，用来对比本地序号是否与官方游戏更新保持一致。
+        返回值为字符串格式，内容为频道与大区的对应列表，适合直接发送为纯文本消息。
+        '''
+        user_name = event.get_sender_name()  # type: ignore # 获取用户的名称
+        user_qq = event.get_sender_id()  # type: ignore # 获取用户的 QQ 号
+        channel, channel_name,date = lucky_channel.lucky_channel(user_qq)  # 计算幸运频道
+        yield event.plain_result(f"{user_name}, 你今天（{date}）的幸运频道是 {channel}（{channel_name}）！")
