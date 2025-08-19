23dc45740a6cc10de3f319b8fd40bba84e73df54
dharc <dharc@outlook.com>
2025-08-20 00:22:07 +0800

    00

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from .core import lucky_channel
 class dnftools(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-
+"""
     @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
     async def on_group_message(self, event: AstrMessageEvent):
         message_str = event.message_str  # 获取消息的纯文本内容
@@ -18,7 +18,7 @@ class dnftools(Star):
             user_qq = event.get_sender_id()  # type: ignore # 获取用户的 QQ 号
             channel, channel_name,date = lucky_channel.lucky_channel(user_qq)  # 计算幸运频道
             yield event.plain_result(f"{user_name}, 你今天（{date}）的幸运频道是 {channel}（{channel_name}）！")
-    
+"""    
     @filter.command("当前频道与大区列表")
     async def list_channels_list(self, event: AstrMessageEvent): # type: ignore
         '''
